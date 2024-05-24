db1792832b438dc01663b95067e22bb8f7c3e5df
OUYANG Zhixin <zxouyang@cpii.hk>
2024-05-24 17:41:19 +0800

    try

--- docs/temp.py
+++ docs/temp.py
@@ -1,1 +1,37 @@
 import pygments
+from docutils import frontend, utils
+from docutils.parsers.rst import Parser
+import re
+settings = frontend.get_default_settings(Parser)
+
+def patch_defi(self, inputstring, document):
+    print(self)
+    import re
+    from docutils.parsers.rst import Parser
+    tag = 'defi_text'
+    pattern = r'\s==(?P<defi_text>\w+)==\s'
+    def to_defi(matched):
+        value = matched.group(tag)
+        return f' :defi:`{value}` '
+
+    return self.parse(re.sub(pattern, to_defi, inputstring), document)
+
+Parser.parse = patch_defi
+    
+    
+with open('temp.rst', encoding='utf-8') as file:
+    document = utils.new_document(file.name, settings)
+    Parser().parse(file.read(), document)
+
+
+print(document.pformat())
+
+
+
+
+
+
+
+
+
+    
---
+++ temp.rst
@@ -0,0 +1,12 @@
+.. role:: defi
+
+Lucy
+=======
+
+我 **最喜歡的** 語言是 Python_。
+
+==mark==
+
+.._Python：https://www.python.org/
+
+Some :defi:`colored text` at last!
